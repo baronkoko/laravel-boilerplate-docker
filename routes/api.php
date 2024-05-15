e5ecbcc117bb2ab090cf382c5b259237536e4ec9
Richard Riddik <baronkoko>
2024-05-15 22:12:59 +0300

    add public api route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/test', function (Request $request) {
+    return "DDDD";
+});
